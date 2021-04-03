4421d18ed1b534d9bd61c944dfed191dd922d15d
Sebastian <alu0101131108@ull.edu.es>
2021-04-03 20:23:12 +0100

    extended for n strings lcs search

--- src/main.cpp
+++ src/main.cpp
@@ -3,17 +3,37 @@
 #include "./../include/LCS.hpp"
 int main(int argc, char *argv[])
 {
-  if (argc != 3)
+  if (argc < 3)
   {
-    std::cout << "Error, 3 arguments expected." << std::endl;
-    std::cout << "Usage: ./lcs string1 string2" << std::endl;
-    std::cout << "Desc: It will find all the longest common subsequences between" << std::endl; 
-    std::cout << "string1 and string2, and then output them through the console." << std::endl;
+    std::cout << "Error, n arguments expected, where n >= 2" << std::endl;
+    std::cout << "Usage: ./lcs string1 string2 ... stringN" << std::endl;
+    std::cout << "Desc: It will find all the longest common subsequences between" << std::endl;
+    std::cout << "every string given, and then output them through the console." << std::endl;
   }
-  else 
+  else
   {
     LCS lcs(argv[1], argv[2]);
     std::unordered_set<std::string> LCSs = lcs.getLCSs();
+    int wordsLeft = argc - 3;
+    while (wordsLeft > 0)
+    {
+      std::unordered_set<std::string> preLCSs = LCSs;
+      LCSs.clear();
+      for (const auto &preLcs : preLCSs)
+      {
+        lcs.resetFor(preLcs, argv[wordsLeft + 2]);
+        std::unordered_set<std::string> tmpToAddLCSs = lcs.getLCSs();
+        if (LCSs.empty() || tmpToAddLCSs.empty() || (*LCSs.begin()).size() == (*tmpToAddLCSs.begin()).size())
+        {
+          LCSs.insert(tmpToAddLCSs.begin(), tmpToAddLCSs.end());
+        }
+        else if ((*LCSs.begin()).size() < (*tmpToAddLCSs.begin()).size())
+        {
+          LCSs = tmpToAddLCSs;
+        }
+      }
+      wordsLeft--;
+    }
     for (const auto &lcs : LCSs)
     {
       std::cout << lcs << std::endl;
